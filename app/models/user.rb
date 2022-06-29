8c44f806c150f6680c4a992eaf6afdbb69374d9b
Dixita <dixitakataria16@gmail.com>
2022-06-29 12:46:33 +0530

    user role added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+         belongs_to :role
 end
