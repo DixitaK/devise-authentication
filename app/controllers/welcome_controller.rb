93a7d1f786a4cfc3e66ef4492d477d1d3a11acfe
Dixita <dixitakataria16@gmail.com>
2022-06-29 12:18:33 +0530

    updated controllers & views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def show
+    @q = User.ransack(params[:q])
+    @users = @q.result
+
+    @users = User.page(params[:page]).per(4)
+
+  end
 end
